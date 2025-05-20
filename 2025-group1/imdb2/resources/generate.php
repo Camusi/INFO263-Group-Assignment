9976e9c74592e17b256da721269e96b20de52300
ItsHammy <itshammy@protonmail.com>
2025-05-20 21:50:27 +1200

    ADD: Generate Pages

--- 2025-group1/imdb2/resources/generate.php
+++ 2025-group1/imdb2/resources/generate.php
@@ -32,5 +32,18 @@ echo '<hr>';
     Currently creating a page for <?php echo isset($_GET['q']) ? htmlspecialchars($_GET['q']) : ' '; ?>.
     Your patience is appreciated and you will be redirected to the page once it is created.
     <br><img src="img/load.gif" alt="Loading..." style="width: 250px; height: 250px;">
+    <hr>
+    <?php
+    if (copy(__DIR__ . '/template_title.php', $pagePath)) {
+        echo "Copying template to {$pagePath}<br>";
+    } else {
+        echo "Failed to copy template to {$pagePath}<br>";
+        $error = error_get_last();
+        if ($error) {
+            echo "Error details: " . htmlspecialchars($error['message']) . "<br>";
+        }
+    }
+
+    ?>
 </body>
 </html>
