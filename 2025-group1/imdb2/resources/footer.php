a4b1a078337cca640bd0cf2df0bba55be620ae17
Hamish <hsa85@uclive.ac.nz>
2025-05-23 10:30:15 +1200

    ADD: Dynamic Footer

---
+++ 2025-group1/imdb2/resources/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <p>&copy; 2025 IMDB2.0. All rights reserved.</p>
+    <p>Contact: <a href="mailto:constatine.zakkaroff@canterbury.ac.nz">Constatine Zakkaroff</a></p>
+    <p>&copy; Made as an assigment for the University of Cantbury. 90 Ilam Road, Christchurch 8011, NZ.</p>
+</footer>
