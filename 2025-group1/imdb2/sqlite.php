04474f62ec65dac46d7f4c1bc5d24b0071f62541
Hamish <hsa85@uclive.ac.nz>
2025-05-23 12:22:11 +1200

    ADD: sqlite.php for simple queries

--- 2025-group1/imdb2/sqlite.php
+++ 2025-group1/imdb2/sqlite.php
@@ -1,4 +1,18 @@
 <?php
+/*
+
+   _____  ____  _      _____ _______ ______   _____  _    _ _____  
+  / ____|/ __ \| |    |_   _|__   __|  ____| |  __ \| |  | |  __ \ 
+ | (___ | |  | | |      | |    | |  | |__    | |__) | |__| | |__) |
+  \___ \| |  | | |      | |    | |  |  __|   |  ___/|  __  |  ___/ 
+  ____) | |__| | |____ _| |_   | |  | |____ _| |    | |  | | |     
+ |_____/ \___\_\______|_____|  |_|  |______(_)_|    |_|  |_|_|     
+                                                                   
+                                                                   
+ This file lets you connect to the SQLite database and perform a simple query.
+ Basically because sqlite3 CLI terminal commands are not installed on GoStudent VMs.
+
+*/
 // SQLite database connection
 try {
     $db = new PDO('sqlite:resources/imdb-2.sqlite3');
