d506a3cbeb3d38bd77ee3c8cf118b1c76ccb54b9
jxadhd <me@joshcross.co.nz>
2025-05-28 14:19:43 +1200

    Likes logic update

--- 2025-group1/imdb2/person/nm11919012.php
+++ 2025-group1/imdb2/person/nm11919012.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{NAME} | IMDB2.0</title>
+    <title>Emma Watson | IMDB2.0</title>
     <link rel="stylesheet" href="../resources/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="../resources/search.js"></script>
@@ -13,25 +13,25 @@
 <body>
     <header class="header">
     <h1>IMDB2.0</h1>
-    <p>{NAME}</p>
+    <p>Emma Watson</p>
   </header>
   <?php include '../resources/navbar.php'; ?>
   <div class="search-results">
     <p id="search-output"></p>
   </div>
     <section class="edit-page-button">
-        <a href="../edit.php?type=person&id={ID}" class="edit-button"><button>Edit This Page</button></a>
+        <a href="../edit.php?type=person&id=nm11919012" class="edit-button"><button>Edit This Page</button></a>
         <div id="warnings">
-            <p id="warning-text">{WARNINGS}</p>
+            <p id="warning-text"><br><span id="page-warning">This page is automatically generated based on data from an IMDB database export. Information may be out of date or not accurately reflect reality.</span><br><br></p>
         </div>
     </section>
     <main class="main-content">
-        <div id="title"><h1>{NAME}</h1></div>
+        <div id="title"><h1>Emma Watson</h1></div>
         <aside id="blurb">
-            <p id="blurb-text">{BLURB}</p>
+            <p id="blurb-text">No biography available yet, but you can add one!</p>
             <ul>
-                <li>Born: <span id="person-year"></span>{YEAR}</span></li>
-                <li>Name: <span id="person-name">{NAME}</span></li>
+                <li>Born: <span id="person-year"></span></span></li>
+                <li>Name: <span id="person-name">Emma Watson</span></li>
             </ul>
         </aside>
         <div id="rating">
@@ -45,7 +45,7 @@
                 }
                 
 
-                $stmt = $db->prepare('SELECT likes FROM name_basics_trim WHERE nconst = \'{ID}\'');
+                $stmt = $db->prepare('SELECT likes FROM name_basics_trim WHERE nconst = \'nm11919012\'');
                 $stmt->execute();
                 $likes = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $likes = $likes[0]['likes'] ?? '?'; // Default to ? if no likes found
@@ -57,7 +57,7 @@
         <div id="roles" class="roles">
             <h2>Roles</h2>
             <ul class="roles-list">
-                {ROLES}
+                <li>No roles found for this person.</li>
             </ul>
         </div>
         <div id="comments"></div>
